9856ae610e036b625d57e326fd2f459f041eeb62
volley-ten <114297417+volley-ten@users.noreply.github.com>
2022-09-24 17:07:18 +0300

    added joomla code block example

---
+++ index.html.php
@@ -0,0 +1,4 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" 
+   xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
